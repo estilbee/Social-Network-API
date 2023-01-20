5f10017aec8f81e292388209af72b1a9f4714cbe
estilbee <odaouphars@gmail.com>
2023-01-20 17:23:19 -0500

    created reaction schema

--- Models/User.js
+++ Models/User.js
@@ -3,8 +3,19 @@ const {Schema,model} = require('mongoose');
 const userSchema = new Schema({
     username: {type: String, required: true, unique: true, trim: true}, 
     email: {type: String, required: true, unique: true, match: /^([A-Za-z0-9_\.-]+)@([\dA-Za-z\.-]+)\.([A-Za-z\.]{2,6})$/ }, 
-    thoughts: [],//array of _id values referencing the Thought model 
-    friends: [],//array of _id values referencing the User model (self referencing)
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thoughts"
+        },
+
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ],
 },
 
 {
