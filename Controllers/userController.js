6821b46331c411dbfdb70cb6912dfd1de97dbaec
estilbee <odaouphars@gmail.com>
2023-01-22 17:44:33 -0500

    added delete and put to userController

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -44,4 +44,31 @@ createUser(req, res) {
 
 },
 
+deleteUser(req, res) {
+    User.findOneAndRemove({_id: req.params.userId})
+    .then((user) => 
+    !user
+        ? res.status(404).json({message: 'No such user exists'})
+        : res.json({message: 'User successfully deleted'})
+    )
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+},
+putUser(req, res) {
+    User.findOneAndUpdate(
+        {_id: req.params.userId},
+        {$addToSet: {thoughts: req.body}},
+        {new: true }
+    )
+    .then((user) => 
+    !user
+        ? res.status(404).json({message:'No user with that ID'})
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+
 }
